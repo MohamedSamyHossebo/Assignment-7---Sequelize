4684ec2db5a7306a9b62388ecd1289c692a2dd3f
Mohamed Samy <116973612+MohamedSamyHossebo@users.noreply.github.com>
2026-02-02 20:17:59 +0200

    format

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -3,9 +3,9 @@ import * as userService from "./users.service.js";
 const router = Router();
 
 router.get("/", userService.getAllUsers);
-router.put("/signup", userService.createOrUpdateUser);
 router.get("/by-email", userService.getUserByEmail);
 router.get("/:id", userService.getUserById);
 router.put("/:id", userService.updateUser);
+router.put("/signup", userService.createOrUpdateUser);
 
 export default router;
