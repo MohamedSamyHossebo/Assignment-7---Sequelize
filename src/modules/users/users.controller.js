00c11ede94dc4e08eeae982230f3402de3b55e4e
Mohamed Samy <116973612+MohamedSamyHossebo@users.noreply.github.com>
2026-02-02 20:01:01 +0200

    feat: get user by id

--- src/modules/users/users.controller.js
+++ src/modules/users/users.controller.js
@@ -5,7 +5,7 @@ const router = Router();
 router.get("/", userService.getAllUsers);
 router.post("/signup", userService.createUser);
 router.get("/by-email", userService.getUserByEmail);
+router.get("/:id", userService.getUserById);
 router.put("/:id", userService.updateUser);
-
 
 export default router;
--- src/modules/users/users.service.js
+++ src/modules/users/users.service.js
@@ -43,6 +43,19 @@ export const getUserByEmail = async (req, res) => {
         return res.status(500).json({ message: error.message })
     }
 }
+export const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await userModel.findOne({ where: { id },paranoid:false });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+        return res.status(200).json({ message: "User fetched successfully", user })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: error.message })
+    }
+}
 export const updateUser = async (req, res) => {
     try {
         const { id } = req.params;
